9047aaaa0343c4857ca866599577b62ff2c13e53
Leo <omarleopaz@hotmail.com>
2019-07-24 21:34:12 -0400

    Changed YoutubeIdInput

--- src/components/YoutubeIdInput/YoutubeIdInput.js
+++ src/components/YoutubeIdInput/YoutubeIdInput.js
@@ -6,49 +6,40 @@ import TextField from '@material-ui/core/TextField';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './YoutubeIdInput.css';
 
-const useStyles = makeStyles(theme => ({
-    container: {
-      display: 'flex',
-      flexWrap: 'wrap',
-    },
-    textField: {
-      marginLeft: theme.spacing(1),
-      marginRight: theme.spacing(1),
-      width: 200,
-    },
-    dense: {
-      marginTop: 19,
-    },
-    menu: {
-      width: 200,
-    },
-  }));
+ 
 
 class YoutubeIdInput extends React.Component {
-    
-    state = {
-        name: 'Cat in the Hat',
-        age: '',
-        multiline: 'Controlled',
-        currency: 'EUR',
-    };
-
     render() {
+      const useStyles = makeStyles(theme => ({
+        container: {
+          display: 'flex',
+          flexWrap: 'wrap',
+        },
+        textField: {
+          marginLeft: theme.spacing(1),
+          marginRight: theme.spacing(1),
+          width: 200,
+        },
+        dense: {
+          marginTop: 19,
+        },
+        menu: {
+          width: 200,
+        },
+      }));
         const classes = useStyles();
-        const handleChange = name => event => {
-            setValues({ ...values, [name]: event.target.value });
-        };
         return (
             <div>
-                <form className={classes.container} noValidate autoComplete="off"/>
+                <form className={classes.container}>
                 <TextField
                     id="standard-name"
                     label="Name"
                     className={classes.textField}
                     value={values.name}
-                    onChange={handleChange('name')}
+                    // onChange={handleChange('name')}
                     margin="normal"
                 />
+                </form>
             </div>
         );
     }
