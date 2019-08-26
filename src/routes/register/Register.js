ce33e9adaa491b4bddc62212178dc8a74367f61b
Leo <omarleopaz@hotmail.com>
2019-08-25 21:30:13 -0400

    Added sign up form with validation

--- src/routes/register/Register.js
+++ src/routes/register/Register.js
@@ -1,32 +1,111 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
-import React from 'react';
 import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Register.css';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import PasswordStr from "./PasswordStr";
+import "./Register.css";
 
-class Register extends React.Component {
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-  };
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+  dense: {
+    marginTop: 19,
+  },
+  menu: {
+    width: 200,
+  },
+}));
 
-  render() {
-    return (
-      <div className={s.root}>
-        <div className={s.container}>
-          <h1>{this.props.title}</h1>
-          <p>...</p>
+function Register({
+  history,
+  onSubmit,
+  onChange,
+  errors,
+  user,
+  score,
+  btnTxt,
+  type,
+  pwMask,
+  onPwChange
+}) {
+  const classes = useStyles();
+  return (
+    <div className={s.root}>
+      <div className={s.container}>
+        <div className="loginBox">
+          <h1>Sign Up</h1>
+          {errors.message && <p style={{ color: "red" }}>{errors.message}</p>}
+
+          <form onSubmit={onSubmit}>
+          <TextField
+            name="username"
+            floatingLabelText="user name"
+            value={user.username}
+            onChange={onChange}
+            errorText={errors.username}
+          />
+          <TextField
+            name="email"
+            floatingLabelText="email"
+            value={user.email}
+            onChange={onChange}
+            errorText={errors.email}
+          />
+          <TextField
+            type={type}
+            name="password"
+            floatingLabelText="password"
+            value={user.password}
+            onChange={onPwChange}
+            errorText={errors.password}
+          />
+        <div className="pwStrRow">
+          {score >= 1 && (
+            <div>
+              <PasswordStr score={score} /> 
+              <Button 
+                className="pwShowHideBtn" 
+                label={btnTxt} onClick={pwMask} 
+                style={{position: 'relative', left: '50%', transform: 'translateX(-50%)'}} 
+              />
+            </div>
+            )} 
         </div>
+        <TextField
+          type={type}
+          name="pwconfirm"
+          floatingLabelText="confirm password"
+          value={user.pwconfirm}
+          onChange={onChange}
+          errorText={errors.pwconfirm}
+        />
+        <br />
+        <Button
+          className="signUpSubmit"
+          primary={true}
+          type="submit"
+          label="submit"
+        />
+      </form>
+      <p>
+        Aleady have an account? <br />
+        <a href="/">Log in here</a>
+      </p>
+    </div>
+        
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default withStyles(s)(Register);
